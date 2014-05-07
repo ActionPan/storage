e8030439c720ecde79b67607a4e6731a6392b167
panruigang <690019756@qq.com>
2014-05-07 11:55:36 +0800

    storage

---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+D:\storage\platforms\android\bin\classes.dex : \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\x07120730\storage\MyStorage.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\x07120730\storage\BuildConfig.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\x07120730\storage\R$attr.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\x07120730\storage\R$drawable.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\x07120730\storage\R$string.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\x07120730\storage\R$xml.class \
+D:\storage\platforms\android\bin\classes\cn\edu\gdmec\x07120730\storage\R.class \
+D:\storage\platforms\android\bin\dexedLibs\classes-0b060cce94409c43e219c5099fbf5c92.jar \
